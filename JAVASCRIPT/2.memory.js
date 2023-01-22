5afcd205b2c18e85190b81df23db6d2795e96dcb
phk0402 <hanafil9742@gmail.com>
2023-01-22 20:01:18 +0900

    return 반환

--- JAVASCRIPT/2.memory.js
+++ JAVASCRIPT/2.memory.js
@@ -1,9 +1,9 @@
 function add(a, b) {
-    console.log(a);
-    console.log(b);
+    // console.log(a);
+    // console.log(b);
     return a+b;
 }
 const sum = add;
 
-// console.log(sum(1, 2));
+console.log(sum(1, 2));
 console.log(add(1, 2));
