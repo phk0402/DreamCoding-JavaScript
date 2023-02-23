58080f6d682835c161f0b1dff925017e0aa5699f
phk0402 <hanafil9742@gmail.com>
2023-02-23 22:34:28 +0900

    array method 2

--- JAVASCRIPT/3.array-method.js
+++ JAVASCRIPT/3.array-method.js
@@ -38,3 +38,48 @@ console.log(fruits); // 배열 자체를 수정(업데이트)
 console.log(deleted);
 fruits.splice(1, 0, '🍉', '🍒');
 console.log(fruits); // 배열 자체를 수정(업데이트)
+
+// 잘라진 새로운 배열을 만듬
+let newArr = fruits.slice(0, 2);
+console.log(newArr);
+console.log(fruits);
+newArr = fruits.slice(-1);
+console.log(newArr);
+
+// 여러개의 배열을 붙여줌
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = arr1.concat(arr2);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+// 순서를 거꾸로
+const arr4 = arr3.reverse();
+console.log(arr4);
+console.clear();
+
+// 중첩 배열을 하나의 배열로 쫙 펴기
+let arr = [
+    [1, 2, 3],
+    [4, [5, 6, [3, 4]]],
+]
+console.log(arr);
+console.log(arr.flat(3));
+arr = arr.flat(3);
+
+// 특정한 값으로 배열을 채우기
+arr.fill(0); // 배열 자체를 수정
+console.log(arr);
+
+arr.fill('s', 1, 3);
+console.log(arr);
+
+arr.fill('a', 1);
+console.log(arr);
+
+// 배열을 문자열로 합하기
+let text = arr.join();
+console.log(text);
+text = arr.join('|');
+console.log(text);
