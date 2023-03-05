1b658136d0dc7d3aad1279bc909aa73228a5dde6
phk0402 <hanafil9742@gmail.com>
2023-03-05 22:26:59 +0900

    iterable quiz

--- JAVASCRIPT/1.iterable.js
+++ JAVASCRIPT/1.iterable.js
@@ -4,11 +4,21 @@
 // 순회가 가능한 객체이다 라는걸 알 수 있음
 // 순회가 가능하면 무엇을 할 수 있나? for..of, spread
 const array = [1, 2, 3];
-for(const item of array.entries()) {
+for (const item of array.entries()) {
     console.log(item);
 }
 
 const obj = {0: 1, 1: 2}
 for (const item in obj) { // key를 출력
     console.log(item);
+}
+
+const iterator = array.values();
+// for(const item of iterator) {
+//     console.log(item);
+// }
+while (true) {
+    const item = iterator.next();
+    if (item.done) break;
+    console.log(item.value);
 }
