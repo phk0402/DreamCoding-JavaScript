d7bab8b757ead6f270bb871832cbb171709f522e
phk0402 <hanafil9742@gmail.com>
2023-03-05 16:06:32 +0900

    hof 고차함수2

--- JAVASCRIPT/6.hof.js
+++ JAVASCRIPT/6.hof.js
@@ -22,19 +22,17 @@ console.log(result);
 result = products.findIndex((value) => value.name === '🍪');
 console.log(result);
 
-// 배열의 아이템들이 부분적으로 조건(콜백함수)에 맞는지 확인
+// some: 배열의 아이템들이 부분적으로 조건(콜백함수)에 맞는지 확인
 result = products.some((item) => item.name === '🍪');
 console.log(result);
 
-// 배열의 아이템들이 전부 조건(콜백함수)에 맞는지 확인
+// every: 배열의 아이템들이 전부 조건(콜백함수)에 맞는지 확인
 result = products.every((item) => item.name === '🍪');
 console.log(result);
 
-// 조건에 맞는 모든 아이템들을 새로운 배열로!
+// filter: 조건에 맞는 모든 아이템들을 새로운 배열로!
 result = products.filter((item) => item.name === '🍪');
 console.log(result);
-
-console.clear();
 
 // Map 배열의 아이템들을 각각 다른 아이템으로 매핑할 수 있는, 변환해서 새로운 배열 생성!
 const nums = [1,2,3,4,5];
@@ -42,16 +40,38 @@ result = nums.map((item) => item * 2);
 console.log(result);
 
 result = nums.map((item) => {
-    if (item % 2 === 0) {
+    if (item % 2 === 0) { // 짝수라면
         return item * 2;
-    } else {
+    } else { // 짝수가 아니라면
         return item;
     }
 });
 console.log(result);
 
 // Flatmap: 중첩된 배열을 쫙 펴줌
+result = nums.map(item => [1, 2]);
+console.log(result);
+
 result = nums.flatMap(item => [1, 2]);
 console.log(result);
 
-// result = ['dream', 'coding'].map(text)
+result = ['dream', 'coding'].flatMap(text => text.split(''));
+console.log(result);
+
+// sort: 배열의 아이템들을 정렬
+// 문자열 형태의 오름차순으로 요소를 정렬하고, 기존의 배열을 변경
+const texts = ['hi', 'abc'];
+texts.sort();
+console.log(texts);
+
+const numbers = [0, 5, 4, 2, 1, 10];
+numbers.sort();
+console.log(numbers);
+// < 0 a가 앞으로 정렬, 오름차순
+// > 0 b가 앞으로 정렬, 내림차순
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+
+// reduce: 배열의 요소들을 접어서 접어서 값을 하나로!
+result = [1, 2, 3, 4, 5].reduce((sum, value) => (sum += value), 0);
+console.log(result);
