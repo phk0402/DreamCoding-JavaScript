5d20f0133a48964d8d3c25a37245237d2e0fc831
phk0402 <hanafil9742@gmail.com>
2023-03-12 14:36:25 +0900

    구조분해 할당 퀴즈

--- JAVASCRIPT/6.destructure.js
+++ JAVASCRIPT/6.destructure.js
@@ -33,3 +33,19 @@ console.log(name);
 console.log(age);
 console.log(occupation);
 console.log(pet);
+
+// Quiz
+const prop = {
+    name: 'Button',
+    styles: {
+        size: 20,
+        color: 'black',
+    },
+};
+
+
+function changeColor({styles: { color }}) {
+    console.log(color);
+}
+
+changeColor(prop);
