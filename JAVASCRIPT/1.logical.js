529c1d1367f40f540b9754ec544c5d4106eca169
phk0402 <hanafil9742@gmail.com>
2023-03-20 23:30:12 +0900

    옵셔널 체이닝

--- JAVASCRIPT/1.logical.js
+++ JAVASCRIPT/1.logical.js
@@ -45,7 +45,7 @@ console.log(obj2);
 // null 또는 undefined인 경우를 확인할 때
 let item = {price: 1};
 const price = item && item.price;
-console.log(price)
+console.log(price);
 
 // 기본값을 설정
 // default parameter 전달하지 않거나, undefined 설정
